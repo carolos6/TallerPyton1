d01037ef1111df8f994483b036ba84a8679305c8
carolos6 <55069690+carolos6@users.noreply.github.com>
2024-07-31 13:31:37 -0500

    Actualizacion

--- ejer_2.py
+++ ejer_2.py
@@ -8,7 +8,9 @@ años= int(input("Ingrese los años:"))
 
 InteresCalculado=cantidad*interes
 
-for i in (1, años+1):
+for i in (1, años):
     InteresTotal=InteresCalculado*i
 
 print("El interes generado Total es de:", InteresTotal)
+print("El interes calculado de aceurdo a los años agendado es de:",InteresTotal)
+print("El interes de generado de acuedo a la capital es de:", InteresCalculado)
